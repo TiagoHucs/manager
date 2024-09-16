6238b4f5dab7ad106f30fe7db31f2ef2727fd499
Tiago S. Hucs <tiagohucs@gmail.com>
2024-09-16 09:10:57 -0300

    ajustando cores e assunto

--- back-end/src/test/java/com/hucs/manager/ManagerApplicationTests.java
+++ back-end/src/test/java/com/hucs/manager/ManagerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hucs.manager;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("","");
 	}
 
 }
