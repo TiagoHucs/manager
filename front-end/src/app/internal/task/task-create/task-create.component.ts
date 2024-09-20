c7d473080db8a0d305cb1085db6ce40684cbcc82
Tiago S. Hucs <tiagohucs@gmail.com>
2024-09-20 13:42:02 -0300

    styles improvemant

--- front-end/src/app/internal/task/task-create/task-create.component.ts
+++ front-end/src/app/internal/task/task-create/task-create.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-create.component.html',
   styleUrl: './task-create.component.css'
 })
 export class TaskCreateComponent {
+  taskForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    
+    this.taskForm = this.fb.group({
+      taskName: ['Minha atividade', [Validators.required]],
+      password: ['123456', [Validators.required]]
+    });
+
+  }
+
+  onSubmit(){
+    console.log(this.taskForm.getRawValue());
+  }
+
 
 }
