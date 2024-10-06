780fafa5c87a8591d2c32550468436ccb7f4b60d
Tiago S. Hucs <tiagohucs@gmail.com>
2024-10-06 16:35:55 -0300

    characters

--- front-end/src/app/interceptors/auth-interceptor.ts
+++ front-end/src/app/interceptors/auth-interceptor.ts
@@ -1,16 +1,16 @@
-import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
+/**
+ * Responsavel apenas por adicionar token nas requisições que nao forem logar ou criar conta
+ */
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const router = inject(Router); // Injetar o Router aqui
-  console.log('Auth Intercepting...')
 
 // Ignora requisições para rotas de autenticação
 if (req.url.includes('/auth')) {
-  console.log('Auth repassou...');
   return next(req);
 }
 
@@ -26,5 +26,4 @@ if (token) {
 } 
 
 return next(authReq);
-
 };
--- front-end/src/app/interceptors/error-interceptor.ts
+++ front-end/src/app/interceptors/error-interceptor.ts
@@ -4,20 +4,22 @@ import { catchError, Observable, throwError } from 'rxjs';
 import { NotifierService } from '../shared/notifier/notifier.service';
 import { Router } from '@angular/router';
 
+/**
+ * Responsavel tratar qualquer eventual erro e avisar o notificador
+ */
 export const errorInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const notifier = inject(NotifierService); // Injetando o serviço dentro do interceptor
   const router = inject(Router); // Injetar o Router aqui
-  console.log('Error Intercepting...')
 
   return next(req).pipe(
     catchError((response: HttpErrorResponse) => {
-      console.log('Error Tratando...')
-      console.log(response)
       if (response.status == 401) {
         const msg = response.error == null || response.error.error == null ? 
         'Usuario não autorizado' : response.error.error;
         notifier.notify('error', msg);
         router.navigate(['/signin']);
+      } else if (response.status == 0){
+        notifier.notify('error', 'Ocorreu um erro inesperado');
       } else {
         notifier.notify('error', response.error.error);
       }
